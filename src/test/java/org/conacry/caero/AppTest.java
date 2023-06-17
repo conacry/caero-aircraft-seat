596ec727dbe845a21ee96c788549fada3c6edd14
Alexey Okruzhko <alex.okruzhko@gmail.com>
2023-06-17 14:12:29 +0600

    feat: Add spring boot init

---
+++ src/test/java/org/conacry/caero/AppTest.java
@@ -0,0 +1,13 @@
+package org.conacry.caero;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertDoesNotThrow(() -> {});
+    }
+}
