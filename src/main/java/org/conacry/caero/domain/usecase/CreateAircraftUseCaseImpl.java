4bb85a9220fb2a3f679f0ae0407bf54137208c61
Zequick1 <yarochkin06@gmail.com>
2023-06-20 19:56:11 +0600

    feat: add create aircraft usecase

--- src/main/java/org/conacry/caero/domain/usecase/CreateAircraftUseCaseImpl.java
+++ src/main/java/org/conacry/caero/domain/usecase/CreateAircraftUseCaseImpl.java
@@ -1,13 +1,41 @@
 package org.conacry.caero.domain.usecase;
 
 import org.conacry.caero.boundary.model.AircraftCreateInfo;
+import org.conacry.caero.boundary.repository.AircraftRepository;
 import org.conacry.caero.boundary.usecase.CreateAircraftUseCase;
 import org.conacry.caero.domain.entity.aircraft.Aircraft;
+import org.conacry.caero.domain.entity.aircraft.AircraftBuilder;
+import org.conacry.caero.domain.entity.aircraft.AircraftID;
+import org.conacry.caero.domain.entity.aircraft.Model;
 
 public class CreateAircraftUseCaseImpl implements CreateAircraftUseCase {
 
+    private final AircraftRepository aircraftRepo;
+
+    public CreateAircraftUseCaseImpl(AircraftRepository aircraftRepo) {
+        this.aircraftRepo = aircraftRepo;
+    }
+
     @Override
     public Aircraft execute(AircraftCreateInfo info) {
-        return null;
+        if (info == null) {
+            throw UseCaseError.errAircraftCreateInfoRequired();
+        }
+
+        var model = Model.from(info.getModel());
+        if (aircraftRepo.existByModel(model)){
+            throw UseCaseError.errAircraftIsAlreadyExisted();
+        }
+
+        var aircraftID = AircraftID.newID();
+        var aircraft = new AircraftBuilder().
+                aircraftID(aircraftID).
+                model(model).
+                build();
+        aircraft.initSeats(info.getSeatConfiguration());
+
+        aircraftRepo.save(aircraft);
+
+        return aircraft;
     }
 }
