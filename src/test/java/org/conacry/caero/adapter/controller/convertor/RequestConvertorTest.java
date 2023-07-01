c8cc8f08bc0a5dd479e69829ba66d682352d320d
Zequick1 <yarochkin06@gmail.com>
2023-07-01 17:54:35 +0600

    feat: request convertor test

--- src/test/java/org/conacry/caero/adapter/controller/convertor/RequestConvertorTest.java
+++ src/test/java/org/conacry/caero/adapter/controller/convertor/RequestConvertorTest.java
@@ -2,6 +2,9 @@ package org.conacry.caero.adapter.controller.convertor;
 
 import org.conacry.caero.adapter.controller.ControllerError;
 import org.conacry.caero.adapter.controller.request.CreateAircraftRequest;
+import org.conacry.caero.boundary.model.SeatConfiguration;
+import org.conacry.caero.domain.entity.aircraft.Aircraft;
+import org.conacry.caero.domain.entity.aircraft.Model;
 import org.conacry.caero.domain.primitive.exception.CodedException;
 import org.junit.jupiter.api.Test;
 
@@ -40,8 +43,8 @@ class RequestConvertorTest {
         businessClassSeatConfig.setRowCount(1);
         businessClassSeatConfig.setSeatsPerRow(2);
         seatConfig.setBusinessSeatInfo(businessClassSeatConfig);
-
-        request.setSeatConfiguration();
+        
+        request.setSeatConfiguration(seatConfig);
 
         var info = RequestConvertor.createRequestToModel(request);
         assertNotNull(info);
